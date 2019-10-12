61e848be22128a7e9b34a663121f87c72eacddce
siegelwang <siegelwang@tencent.com>
2019-10-12 17:53:11 +0800

    完成正确的OCaml的finally tagless的实现

--- design_patterns/exp_finally_tagless_exhaustive.ml
+++ design_patterns/exp_finally_tagless_exhaustive.ml
@@ -1,15 +1,14 @@
 exception BadResult of string
 
 type exp =
-  [ `Int of int | `Negate of exp | `Add of exp * exp]
+  [`Int of int
+  | `Negate of exp
+  | `Add of exp * exp]
 
-let rec eval e =
-  match e with
-  | `Int _ -> e
-  | `Negate e1 -> (match eval e1 with
-                   | `Int i -> `Int (-i))
-  | `Add(e1, e2) -> (match (eval e1, eval e2) with
-                     | (`Int i, `Int j) -> `Int (i + j))
+let rec eval  = function
+  | `Int i -> i
+  | `Negate e ->  -(eval e)
+  | `Add(e1, e2) -> (eval e1 ) + (eval e2)
 
 let rec toString = function
   | `Int i -> string_of_int i
@@ -18,12 +17,9 @@ let rec toString = function
 
 type new_exp = [ exp | `Sub of new_exp * new_exp]
 
-
-let rec new_eval e =
-  match e with
+let rec new_eval : new_exp -> int = function
   | #exp as exp -> eval exp
-  | `Sub(e1, e2) -> (match (new_eval e1, new_eval e2) with
-                     | (`Int i, `Int j) -> `Int (i - j))
+  | `Sub(e1, e2) -> (new_eval e1) - (new_eval e2)
 
 let rec new_toString : new_exp -> string = function
   | `Sub(e1, e2) -> "(" ^ (new_toString e1) ^ "-" ^ (new_toString e2) ^ ")"
@@ -31,11 +27,14 @@ let rec new_toString : new_exp -> string = function
 
 ;;
 
-let a = `Int 7
+let a = `Int 10
 let b = `Int 6
 let c = `Sub(a, b)
-let d = new_eval c;;
+let d = new_eval c
+;;
+print_endline (string_of_int d);;
 
-let res = toString d;;
-print_endline (new_toString c);;
+let res = toString (`Add ((`Negate (`Int 5)), (`Int 6)));;
+let num = eval (`Add ((`Negate (`Int 5)), (`Int 6)));;
 print_endline res;;
+print_endline (string_of_int num);;
