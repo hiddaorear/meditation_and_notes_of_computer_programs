090f7da435bcf90c6e1cd848a2a8e6ee23371cec
Siegel Wang <hiddaorear@outlook.com>
2019-10-14 01:49:50 +0800

    整理expression problem代码

--- expression_problem/exp.ml
+++ expression_problem/exp.ml
@@ -1,25 +1,22 @@
-exception BadResult of string
-
 type exp =
-  | Int of int
-  | Negate of exp
-  | Add of exp * exp
+  Int of int
+| Negate of exp
+| Add of exp * exp
 
-let rec eval e =
-  match e with
-  | Int _ -> e
-  | Negate e1 -> (match eval e1 with
-                  | Int i -> Int(-i)
-                  | _ -> raise (BadResult "non-int in negation"))
-  | Add(e1, e2) -> (match (eval e1, eval e2) with
-                    | (Int i, Int j) -> Int (i + j)
-                    | _ -> raise (BadResult "non-ints in addition"))
+let rec eval  = function
+  | Int i -> i
+  | Negate e ->  -(eval e)
+  | Add(e1, e2) -> (eval e1 ) + (eval e2)
 
 let rec toString = function
   | Int i -> string_of_int i
   | Negate e -> "-(" ^ (toString e) ^ ")"
   | Add(e1, e2)  -> "(" ^ (toString e1) ^ "+" ^ (toString e2) ^ ")"
+
 ;;
 
-let res = toString (eval (Add ((Negate (Int 5)), (Int 6))));;
+
+let res = toString (Add ((Negate (Int 5)), (Int 6)));;
+let num = eval (Add ((Negate (Int 5)), (Int 6)));;
 print_endline res;;
+print_endline (string_of_int num);;
